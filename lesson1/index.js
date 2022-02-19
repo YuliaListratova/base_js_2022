214592e80cddc06ad41e0c7d96cef4df6d093812
Julia Listratova <juliazyuzin@yandex.ru>
2022-02-19 11:20:43 +0300

    Homework 1

---
+++ lesson1/index.js
@@ -0,0 +1,27 @@
+console.log("HELLO JAVASCRIPT");
+
+// Homework 1 
+
+//Переменная для “названия нашей планеты”
+const ourPlanetName="Earth";
+console.log(ourPlanetName);
+
+//Переменная для “текущее время пользователя”
+let currentTime = new Date ();
+console.log(currentTime.toTimeString());
+
+//Переменная которая показывает “количества статей”
+let numberOfArticle = 147;
+console.log(numberOfArticle);
+
+//Переменная которая показывает “это оплата наличными деньгами или нет”
+let isCash = true;
+console.log(isCash);
+
+//Три переменные для хранения Ф.И.О
+let myLastName = "Listratova";
+console.log(myLastName)
+const myFirstName = "Yulia";
+console.log(myFirstName);
+const myMiddleName = "Victorovna";
+console.log(myMiddleName);
