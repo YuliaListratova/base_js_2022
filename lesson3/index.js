c7b91fcc4237a91a78e63df355759d3380d27b82
Julia Listratova <juliazyuzin@yandex.ru>
2022-02-26 21:38:57 +0300

    lesson3

---
+++ lesson3/index.js
@@ -0,0 +1,102 @@
+//Примитивные типы данных:
+
+//Number
+//console.log(1 / 0);
+//console.log("Alex"/0);
+//console.log(typeof NaN);
+
+//String
+// const myString = "строка";
+// console.log(myString);
+// const anotherString = 'текст: "текст" текст';
+// const templateString = `шаблонная строка ${anotherString}`;
+// console.log(templateString);
+
+//Boolean
+// const isAdmin = true;
+// const isUser = false;
+
+//Null
+// const emptyVariable = null;
+
+//Undefined
+// const userName ;
+// console.log(userName);
+
+//Непримитивные типы данных
+
+//Object
+// const someName = "Petya"
+// const user = {
+//   name: someName,
+//   lastName: "Ivanov",
+//   age: 23,
+//   citizenship: "Russia",
+//   newObject: {
+//       newProperty: "property"
+//   }
+// };
+// console.log(user.newObject.newProperty);
+
+//Symbol
+//не используют
+
+//Bigint
+//сверх большие цифры
+
+// Оператор typeof
+// typeof null - всегда объект! Ошибка JS
+// console.log(typeof null);
+
+//Преобразование типов
+
+//Строковое преобразование
+// const myVar = trye;
+// const myVarToString = String(myVar);
+// console.log(myVarToString);
+
+//Численное преобразование
+//  const myVar = "assd123";
+//  const myVarToNumber = Number(myVar);
+//  console.log(myVarToNumber);
+
+// Логическое преобразование
+//  const myVar = 1;
+//  console.log(!!myVar);
+
+//Математические операторы
+//Бинарный плюс
+// const myNumber = 1;
+// const newNamber = 2;
+// const myString = "строка";
+// console.log(myNumber + myString);
+
+//Сравнение строк
+// const string1 = "строка";
+// const string2 = "солнце";
+// console.log(string1 == string2);
+// console.log(string1 > string2);
+
+//unicode
+
+// const userTypes = {
+//     admin: "admin",
+//     user: "user",
+//     teacher: "teacher",
+//     contentManager: "contentManager",
+// };
+// const serverDateUserType = "user";
+
+// if (serverDateUserType === userTypes.admin) {
+//     console.log("показать весь контент");
+// }else {
+//     console.log("показатьвесь ограниченный контент");
+// }
+
+// const myNumber = 1;
+// const myString = "1";
+// console.log(myNumber == myString)
+
+// console.log(null == "\n0\n");
+
+console.log("\t\n"-2);
