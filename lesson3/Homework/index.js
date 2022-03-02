625a61048fcb71b2ce3f3dbf67cefff2f2c3c1cc
Julia Listratova <juliazyuzin@yandex.ru>
2022-03-02 23:10:00 +0300

    add homework

---
+++ lesson3/Homework/index.js
@@ -0,0 +1,41 @@
+// 1 Вывести в терминал строку “Hello World!”
+console.log("Hello World");
+
+
+// Создать переменную для каждого типа данных
+// Вывести в консоль редактора кода созданные переменные
+
+// Number
+let numberOfTasks = 10;
+console.log(numberOfTasks);
+
+// BigInt
+const bigInt = 1234567890123456789012345678901234567890n;
+console.log(bigInt);
+
+// String
+let userName = "Андрей";
+console.log(userName);
+
+// Boolean
+let isAdmin = true;
+console.log(isAdmin);
+
+// Null
+let lastName = null;
+console.log(lastName);
+
+// Undefined
+let firstName;
+console.log(firstName);
+
+// Object
+let book = {
+    title: "Война и мир",
+    author: "Л.Н. Толстой",
+    quote: "Надо жить, надо любить, надо верить.",
+}
+console.log(book.quote);
+
+//Symbol
+//не используют
